897505586cbb47ee0644d2a74ec9dc8f9d3e03cb
Wyatt Barnes <wyatt@writerof.software>
2020-03-27 08:49:47 -0500

    Update handler to work with generic deposit method in Bridge

--- contracts/handlers/CentrifugeAssetHandler.sol
+++ contracts/handlers/CentrifugeAssetHandler.sol
@@ -1,38 +1,72 @@
-// pragma solidity 0.6.4;
+pragma solidity 0.6.4;
 
-// import "../interfaces/IDepositHandler.sol";
+import "../interfaces/IDepositHandler.sol";
 
-// contract CentrifugeAssetHandler is IDepositHandler {
-//     address public _bridgeAddress;
+contract CentrifugeAssetHandler is IDepositHandler {
+    address public _bridgeAddress;
 
-//     enum AssetDepositStatus { Uninitialized, Active, Confirmed }
+    enum AssetDepositStatus { Uninitialized, Active, Confirmed }
 
-//     // metaDataHash => AssetDepositStatus
-//     mapping(bytes32 => AssetDepositStatus) _assetDepositStatuses;
+    struct DepositRecord {
+        address originChainContractAddress;
+        uint256 destinationChainID;
+        address destinationChainHandlerAddress;
+        address destinationRecipientAddress;
+        address depositer;
+        bytes32 metaDataHash;
+    }
 
-//     modifier _onlyBridge() {
-//         require(msg.sender == _bridgeAddress, "sender must be bridge contract");
-//         _;
-//     }
+    // depositID => DepositRecord
+    mapping(uint256 => DepositRecord) _depositRecords;
+    // metaDataHash => AssetDepositStatus
+    mapping(bytes32 => AssetDepositStatus) _assetDepositStatuses;
 
-//     constructor(address bridgeAddress) public {
-//         _bridgeAddress = bridgeAddress;
-//     }
+    modifier _onlyBridge() {
+        require(msg.sender == _bridgeAddress, "sender must be bridge contract");
+        _;
+    }
 
-//     function depositAsset(bytes32 metaDataHash) public _onlyBridge {
-//         require(_assetDepositStatuses[metaDataHash] == AssetDepositStatus.Uninitialized,
-//         "asset has already been deposited and cannot be changed");
-//         _assetDepositStatuses[metaDataHash] = AssetDepositStatus.Active;
-//     }
+    constructor(address bridgeAddress) public {
+        _bridgeAddress = bridgeAddress;
+    }
 
-//     function executeDeposit(bytes memory data) public override {
-//         bytes32 metaDataHash;
+    function deposit(uint256 depositID, address depositer, bytes memory data) public override _onlyBridge {
+        address originChainContractAddress;
+        uint256 destinationChainID;
+        address destinationChainHandlerAddress;
+        address destinationRecipientAddress;
+        bytes32 metaDataHash;
 
-//         assembly {
-//             metaDataHash := mload(add(data, 0x20))
-//         }
+        assembly {
+            originChainContractAddress     := mload(add(data,0x20))
+            destinationChainID             := mload(add(data,0x40))
+            destinationChainHandlerAddress := mload(add(data,0x60))
+            destinationRecipientAddress    := mload(add(data,0x80))
+            metaDataHash                   := mload(add(data,0xA0))
+        }
 
-//         require(_assetDepositStatuses[metaDataHash] == AssetDepositStatus.Active, "asset hasn't been deposited or has already been finalized");
-//         _assetDepositStatuses[metaDataHash] = AssetDepositStatus.Confirmed;
-//     }
-// }
+        require(_assetDepositStatuses[metaDataHash] == AssetDepositStatus.Uninitialized,
+        "asset has already been deposited and cannot be changed");
+        _assetDepositStatuses[metaDataHash] = AssetDepositStatus.Active;
+
+        _depositRecords[depositID] = DepositRecord(
+            originChainContractAddress,
+            destinationChainID,
+            destinationChainHandlerAddress,
+            destinationRecipientAddress,
+            depositer,
+            metaDataHash
+        );
+    }
+
+    function executeDeposit(bytes memory data) public override {
+        bytes32 metaDataHash;
+
+        assembly {
+            metaDataHash := mload(add(data, 0x20))
+        }
+
+        require(_assetDepositStatuses[metaDataHash] == AssetDepositStatus.Active, "asset hasn't been deposited or has already been finalized");
+        _assetDepositStatuses[metaDataHash] = AssetDepositStatus.Confirmed;
+    }
+}

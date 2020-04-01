31b521ce77df3e9b50a1c8974cccb835bbe2e497
Wyatt Barnes <wyatt@writerof.software>
2020-04-01 00:52:44 -0500

    Update executeDeposit

--- contracts/handlers/ERC20Handler.sol
+++ contracts/handlers/ERC20Handler.sol
@@ -72,7 +72,7 @@ contract ERC20Handler is IDepositHandler, ERC20Safe {
 
             IBridge bridge = IBridge(_bridgeAddress);
             uint chainID = bridge._chainID();
-        
+
             tokenID = createTokenID(chainID, originChainTokenAddress);
 
              _tokenContractAddressToTokenID[originChainTokenAddress] = tokenID;
@@ -92,24 +92,113 @@ contract ERC20Handler is IDepositHandler, ERC20Safe {
         );
     }
 
-    function createTokenID(uint256 chainID, address originChainTokenAddress) internal returns (string memory) {
+    function createTokenID(uint256 chainID, address originChainTokenAddress) internal pure returns (string memory) {
         return string(abi.encodePacked(chainID, originChainTokenAddress));
     }
 
-    // TODO If any address can call this, anyone can mint tokens
+    // Copied from https://github.com/provable-things/ethereum-api/blob/master/oraclizeAPI_0.5.sol#L1020
+    function parseInt(string memory _a, uint _b) internal pure returns (uint _parsedInt) {
+        bytes memory bresult = bytes(_a);
+        uint mint = 0;
+        bool decimals = false;
+        for (uint i = 0; i < bresult.length; i++) {
+            if ((uint(uint8(bresult[i])) >= 48) && (uint(uint8(bresult[i])) <= 57)) {
+                if (decimals) {
+                   if (_b == 0) {
+                       break;
+                   } else {
+                       _b--;
+                   }
+                }
+                mint *= 10;
+                mint += uint(uint8(bresult[i])) - 48;
+            } else if (uint(uint8(bresult[i])) == 46) {
+                decimals = true;
+            }
+        }
+        if (_b > 0) {
+            mint *= 10 ** _b;
+        }
+        return mint;
+    }
+
+    // copied from https://github.com/provable-things/ethereum-api/blob/master/oraclizeAPI_0.5.sol#L872
+    function parseAddr(string memory _a) internal pure returns (address _parsedAddress) {
+        bytes memory tmp = bytes(_a);
+        uint160 iaddr = 0;
+        uint160 b1;
+        uint160 b2;
+        for (uint i = 2; i < 2 + 2 * 20; i += 2) {
+            iaddr *= 256;
+            b1 = uint160(uint8(tmp[i]));
+            b2 = uint160(uint8(tmp[i + 1]));
+            if ((b1 >= 97) && (b1 <= 102)) {
+                b1 -= 87;
+            } else if ((b1 >= 65) && (b1 <= 70)) {
+                b1 -= 55;
+            } else if ((b1 >= 48) && (b1 <= 57)) {
+                b1 -= 48;
+            }
+            if ((b2 >= 97) && (b2 <= 102)) {
+                b2 -= 87;
+            } else if ((b2 >= 65) && (b2 <= 70)) {
+                b2 -= 55;
+            } else if ((b2 >= 48) && (b2 <= 57)) {
+                b2 -= 48;
+            }
+            iaddr += (b1 * 16 + b2);
+        }
+        return address(iaddr);
+    }
+
+    // copied from https://ethereum.stackexchange.com/questions/31457/substring-in-solidity
+    function subString(string memory str, uint startIndex, uint endIndex) internal pure returns (string memory) {
+        bytes memory strBytes = bytes(str);
+        bytes memory result = new bytes(endIndex-startIndex);
+        for(uint i = startIndex; i < endIndex; i++) {
+            result[i-startIndex] = strBytes[i];
+        }
+        return string(result);
+    }
+
     function executeDeposit(bytes memory data) public override _onlyBridge {
-        address destinationChainTokenAddress;
-        address destinationRecipientAddress;
-        uint256 amount;
+        string memory tokenID;
+        address       destinationRecipientAddress;
+        uint256       amount;
 
         assembly {
-            destinationChainTokenAddress := mload(add(data, 0x20))
-            destinationRecipientAddress  := mload(add(data, 0x40))
-            amount                       := mload(add(data, 0x60))
+            tokenID                     := mload(add(data, 0x20))
+            destinationRecipientAddress := mload(add(data, 0x40))
+            amount                      := mload(add(data, 0x60))
         }
 
-        ERC20Mintable erc20 = ERC20Mintable(destinationChainTokenAddress);
-        erc20.mint(destinationRecipientAddress, amount);
+        if (_tokenIDToTokenContractAddress[tokenID] != address(0)) {
+            // token exists
+            uint256 tokenChainID = parseInt(subString(tokenID, 0, 1), 0);
+            address tokenAddress = parseAddr(subString(tokenID, 2, bytes(tokenID).length));
+
+            IBridge bridge = IBridge(_bridgeAddress);
+            uint256 chainID = bridge._chainID();
+
+            if (tokenChainID == chainID) {
+                // token is from same chain
+                ERC20Mintable erc20 = ERC20Mintable(tokenAddress);
+                erc20.transferFrom(address(this), destinationRecipientAddress, amount);
+            } else {
+                // token is not from chain
+                ERC20Mintable erc20 = ERC20Mintable(tokenAddress);
+                erc20.mint(destinationRecipientAddress, amount);
+            }
+        } else {
+            // Token doesn't exist
+            ERC20Mintable erc20 = new ERC20Mintable();
+
+            // Create a relationship between the originAddress and the synthetic
+            _tokenIDToTokenContractAddress[tokenID] = address(erc20);
+            _tokenContractAddressToTokenID[address(erc20)] = tokenID;
+
+            erc20.mint(destinationRecipientAddress, amount);
+        }
     }
 
     function withdraw(address tokenAddress, address recipient, uint amount) public _onlyBridge {

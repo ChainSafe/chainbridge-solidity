6bfdd32d3498abdc0b7eb8269885b41c8459493c
David Ansermino <david@chainsafe.io>
2020-04-07 14:40:31 -0400

    Updates readme, disables discovery

--- scripts/geth/start_geth.sh
+++ scripts/geth/start_geth.sh
@@ -16,7 +16,7 @@ cp -r ./scripts/geth/keystore $DATADIR
 # Start geth with rpc, mining and unlocked accounts
 
 if [[ $QUIET ]]; then
-    geth --verbosity 2 --datadir $DATADIR --rpcport 8545 --unlock "0xff93B45308FD417dF303D6515aB04D9e89a750Ca","0x8e0a907331554AF72563Bd8D43051C2E64Be5d35","0x24962717f8fA5BA3b931bACaF9ac03924EB475a0","0x148FfB2074A9e59eD58142822b3eB3fcBffb0cd7","0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485" --password ./scripts/geth/password.txt --rpc --networkid 5 --rpccorsdomain="*" --targetgaslimit 8000000 --allow-insecure-unlock --mine
+    geth --verbosity 2 --datadir $DATADIR --rpcport 8545 --nodiscover --unlock "0xff93B45308FD417dF303D6515aB04D9e89a750Ca","0x8e0a907331554AF72563Bd8D43051C2E64Be5d35","0x24962717f8fA5BA3b931bACaF9ac03924EB475a0","0x148FfB2074A9e59eD58142822b3eB3fcBffb0cd7","0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485" --password ./scripts/geth/password.txt --rpc --networkid 5 --rpccorsdomain="*" --targetgaslimit 8000000 --allow-insecure-unlock --mine
 else
-    geth --datadir $DATADIR --rpcport 8545 --unlock "0xff93B45308FD417dF303D6515aB04D9e89a750Ca","0x8e0a907331554AF72563Bd8D43051C2E64Be5d35","0x24962717f8fA5BA3b931bACaF9ac03924EB475a0","0x148FfB2074A9e59eD58142822b3eB3fcBffb0cd7","0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485" --password ./scripts/geth/password.txt --rpc --networkid 5 --rpccorsdomain="*" --targetgaslimit 8000000 --allow-insecure-unlock --mine
+    geth --datadir $DATADIR --rpcport 8545 --nodiscover --unlock "0xff93B45308FD417dF303D6515aB04D9e89a750Ca","0x8e0a907331554AF72563Bd8D43051C2E64Be5d35","0x24962717f8fA5BA3b931bACaF9ac03924EB475a0","0x148FfB2074A9e59eD58142822b3eB3fcBffb0cd7","0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485" --password ./scripts/geth/password.txt --rpc --networkid 5 --rpccorsdomain="*" --targetgaslimit 8000000 --allow-insecure-unlock --mine
 fi

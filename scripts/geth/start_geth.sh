8babcd9c4990054608cabf523adf5c5731eed138
David Ansermino <david@chainsafe.io>
2020-04-07 10:33:26 -0400

    Updates signers

--- scripts/geth/start_geth.sh
+++ scripts/geth/start_geth.sh
@@ -11,6 +11,7 @@ rm -rf $DATADIR
 # Init genesis
 geth init ./scripts/geth/genesis.json --datadir $DATADIR
 # Copy keystore
-cp ./scripts/geth/UTC--2019-02-01T20-30-17.296Z--f6016af49e2764fd9ae5188b8cd099dd0ccb86b0 $DATADIR/keystore/
+rm -rf $DATADIR/keystore
+cp -r ./scripts/geth/keystore $DATADIR
 # Start geth with rpc, mining and unlocked account
-geth --datadir $DATADIR --rpcport 8545 --unlock 0xF6016af49e2764Fd9AE5188b8CD099dd0CCb86b0 --password ./scripts/geth/password.txt --rpc --networkid 5 --rpccorsdomain="*" --targetgaslimit 8000000 --allow-insecure-unlock --mine
+geth --datadir $DATADIR --rpcport 8545 --unlock     "0xff93B45308FD417dF303D6515aB04D9e89a750Ca","0x8e0a907331554AF72563Bd8D43051C2E64Be5d35","0x24962717f8fA5BA3b931bACaF9ac03924EB475a0","0x148FfB2074A9e59eD58142822b3eB3fcBffb0cd7","0x4CEEf6139f00F9F4535Ad19640Ff7A0137708485" --password ./scripts/geth/password.txt --rpc --networkid 5 --rpccorsdomain="*" --targetgaslimit 8000000 --allow-insecure-unlock --mine

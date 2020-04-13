9d0b45d2e43656251dda92c21cbe5412852b3b22
steviezhang <stephanie@chainsafe.io>
2020-04-13 18:15:38 -0400

    updated all handlers to utilize whitelist

--- contracts/handlers/CentrifugeAssetHandler.sol
+++ contracts/handlers/CentrifugeAssetHandler.sol
@@ -27,13 +27,50 @@ contract CentrifugeAssetHandler is IDepositHandler {
     // metaDataHash => AssetDepositStatus
     mapping(bytes32 => bool) _assetDepositStatuses;
 
+    // token contract address => is whitelisted
+    mapping (address => bool) public _contractWhitelist;
+
     modifier _onlyBridge() {
         require(msg.sender == _bridgeAddress, "sender must be bridge contract");
         _;
     }
 
-    constructor(address bridgeAddress) public {
+    constructor(
+        address bridgeAddress,
+        bytes32[] memory initialResourceIDs,
+        address[] memory initialContractAddresses
+        
+    ) public {
+        require(initialResourceIDs.length == initialContractAddresses.length,
+            "mismatch length between initialResourceIDs and initialContractAddresses");
+
         _bridgeAddress = bridgeAddress;
+
+        for (uint256 i = 0; i < initialResourceIDs.length; i++) {
+            _setResourceIDAndContractAddress(initialResourceIDs[i], initialContractAddresses[i]);
+        }
+    }
+
+    function isWhitelisted(address contractAddress) internal view returns (bool) {
+        return _contractWhitelist[contractAddress];
+    }
+
+    function _setResourceIDAndContractAddress(bytes32 resourceID, address contractAddress) internal {
+        _resourceIDToTokenContractAddress[resourceID] = contractAddress;
+        _tokenContractAddressToResourceID[contractAddress] = resourceID;
+
+        _contractWhitelist[contractAddress] = true;
+    }
+
+    function setResourceIDAndContractAddress(bytes32 resourceID, address contractAddress) public {
+        require(_resourceIDToTokenContractAddress[resourceID] == address(0), "resourceID already has a corresponding contract address");
+
+        bytes32 currentResourceID = _tokenContractAddressToResourceID[contractAddress];
+        bytes32 emptyBytes;
+        require(keccak256(abi.encodePacked((currentResourceID))) == keccak256(abi.encodePacked((emptyBytes))),
+            "contract address already has corresponding resourceID");
+
+        _setResourceIDAndContractAddress(resourceID, contractAddress);
     }
 
     function getDepositRecord(uint256 depositID) public view returns (DepositRecord memory) {
@@ -62,34 +99,42 @@ contract CentrifugeAssetHandler is IDepositHandler {
     // destinationRecipientAddress                bytes32   bytes    32 - 64
     // metadataHash                               bytes     bytes    64 - 96
     function deposit(uint8 originChainID, uint256 depositNonce, address depositer, bytes memory data) public override _onlyBridge {
-        address originChainContractAddress;
+        // address originChainContractAddress;
+        bytes32 resourceID;
         address destinationRecipientAddress;
         bytes32 metaDataHash;
 
         assembly {
-            originChainContractAddress     := mload(add(data,0x20))
+            // originChainContractAddress     := mload(add(data,0x20))
+            resourceID                     := mload(add(data,0x20))
             destinationRecipientAddress    := mload(add(data,0x40))
             metaDataHash                   := mload(add(data,0x60))
         }
 
-        bytes32      resourceID = _tokenContractAddressToResourceID[originChainContractAddress];
-        bytes32      emptyBytes;
+        address originChainContractAddress = _resourceIDToTokenContractAddress[resourceID];
+        require(isWhitelisted(originChainContractAddress), "provided originChainContractAddress is not whitelisted");
 
-        if (keccak256(abi.encodePacked((resourceID))) == keccak256(abi.encodePacked((emptyBytes)))) {
-            // The case where we have never seen this token address before
+        // we are currently only allowing for interactions with whitelisted tokenContracts
+        // there should not be a case where we recieve an empty resourceID
 
-            // If we have never seen this token and someone was able to perform a deposit,
-            // it follows that the token is native to the current chain.
+        // bytes32      resourceID = _tokenContractAddressToResourceID[originChainContractAddress];
+        // bytes32      emptyBytes;
 
-            IBridge bridge = IBridge(_bridgeAddress);
-            uint8 chainID = uint8(bridge._chainID());
+        // if (keccak256(abi.encodePacked((resourceID))) == keccak256(abi.encodePacked((emptyBytes)))) {
+        //     // The case where we have never seen this token address before
 
-            resourceID = createResourceID(originChainContractAddress,chainID);
+        //     // If we have never seen this token and someone was able to perform a deposit,
+        //     // it follows that the token is native to the current chain.
 
-             _tokenContractAddressToResourceID[originChainContractAddress] = resourceID;
-             _resourceIDToTokenContractAddress[resourceID] = originChainContractAddress;
+        //     IBridge bridge = IBridge(_bridgeAddress);
+        //     uint8 chainID = uint8(bridge._chainID());
 
-        }
+        //     resourceID = createResourceID(originChainContractAddress,chainID);
+
+        //      _tokenContractAddressToResourceID[originChainContractAddress] = resourceID;
+        //      _resourceIDToTokenContractAddress[resourceID] = originChainContractAddress;
+
+        // }
 
         require(_assetDepositStatuses[metaDataHash] == false,
         "asset has already been deposited and cannot be changed");
@@ -109,13 +154,16 @@ contract CentrifugeAssetHandler is IDepositHandler {
 
     // metadataHash                               bytes     bytes    0 - 32
     function executeDeposit(bytes memory data) public override _onlyBridge {
-        // bytes32 resourceID;
+        bytes32 resourceID;
         bytes32 metaDataHash;
 
         assembly {
-            // resourceID := mload(add(data, 0x20))
-            metaDataHash := mload(add(data, 0x20))
+            resourceID := mload(add(data, 0x20))
+            metaDataHash := mload(add(data, 0x40))
         }
+
+        require(isWhitelisted(address(_resourceIDToTokenContractAddress[resourceID])), "provided tokenAddress is not whitelisted");
+
 
         // bytes20 originChainContractAddress;
 

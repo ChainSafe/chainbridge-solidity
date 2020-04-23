2dd9b2aa07bbb93e21abc228ec99a884d0466fee
David Ansermino <david@chainsafe.io>
2020-04-23 17:21:56 -0400

    Replace resource ID lookup with burnlist (#89)
    - Use burnlist to decide whether to mint
    - Updates tests to use mint/burn

--- contracts/handlers/ERC721Handler.sol
+++ contracts/handlers/ERC721Handler.sol
@@ -114,7 +114,6 @@ contract ERC721Handler is IDepositHandler, ERC721Safe {
     // metadata                        length      uint256    bytes    (96 + len(destinationRecipientAddress)) - (96 + len(destinationRecipientAddress) + 32)
     // metadata                                      bytes    bytes    (96 + len(destinationRecipientAddress) + 32) - END
     function deposit(uint8 destinationChainID, uint256 depositNonce, address depositer, bytes memory data) public override _onlyBridge {
-        // address      originChainTokenAddress;
         bytes32      resourceID;
         uint         lenDestinationRecipientAddress;
         uint         tokenID;
@@ -279,13 +278,10 @@ contract ERC721Handler is IDepositHandler, ERC721Safe {
         IBridge bridge = IBridge(_bridgeAddress);
         uint8 chainID = bridge._chainID();
 
-        if (uint8(resourceID[31]) == chainID) {
-            // token is from same chain
-            releaseERC721(tokenAddress, address(this), address(recipientAddress), tokenID);
-        } else {
-            // token is not from chain
-
+        if (_burnList[tokenAddress]) {
             mintERC721(tokenAddress, address(recipientAddress), tokenID, metaData);
+        } else {
+            releaseERC721(tokenAddress, address(this), address(recipientAddress), tokenID);
         }
 
         // As we are only allowing for interaction with whitelisted contracts, this case no longer exists

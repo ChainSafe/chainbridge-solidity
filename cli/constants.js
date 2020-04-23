546e24200d1198d84a4c2a40701d0322b26dbbfc
Wyatt Barnes <wyatt@writerof.software>
2020-04-23 18:10:38 -0500

    Debugging

--- cli/constants.js
+++ cli/constants.js
@@ -11,9 +11,9 @@ const ContractABIs = {
     Erc20Handler: require(CONTRACT_PATH + "build/contracts/ERC20Handler.json"),
     Erc20Mintable: require(CONTRACT_PATH + "build/contracts/ERC20PresetMinterPauser.json"),
     Erc721Handler: require(CONTRACT_PATH + "build/contracts/ERC721Handler.json"),
-    Erc721Mintable: require(CONTRACT_PATH + "build/contracts/ERC721Mintable.json"),
-    // CentrifugeHandler: require(CONTRACT_PATH + "build/contracts/CentrifugeAssetHandler.json")
+    Erc721Mintable: require(CONTRACT_PATH + "build/contracts/ERC721Mintable.json")
 }
+// CentrifugeHandler: require(CONTRACT_PATH + "build/contracts/CentrifugeAssetHandler.json")
 
 module.exports.ContractABIs = ContractABIs
 // This is just Alice's key.

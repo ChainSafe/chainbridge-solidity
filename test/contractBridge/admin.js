cba4128e3daba07024bb93e49fc6722096ea7df6
Wyatt Barnes <wyatt@writerof.software>
2020-04-24 03:28:41 -0500

    Finish adding admin method tests

--- test/contractBridge/admin.js
+++ test/contractBridge/admin.js
@@ -3,9 +3,12 @@
  * SPDX-License-Identifier: LGPL-3.0-only
  */
 const TruffleAssert = require('truffle-assertions');
+const Ethers = require('ethers');
 
 const RelayerContract = artifacts.require("Relayer");
 const BridgeContract = artifacts.require("Bridge");
+const ERC20MintableContract = artifacts.require("ERC20PresetMinterPauser");
+const ERC20HandlerContract = artifacts.require("ERC20Handler");
 
 // This test does NOT include all getter methods, just 
 // getters that should work with only the constructor called
@@ -61,6 +64,14 @@ contract('Bridge - [admin]', async accounts => {
         assert.isTrue(await RelayerInstance.isRelayer(newRelayer));
     });
 
+    it('newRelayer should be removed as a relayer after being added', async () => {
+        const newRelayer = accounts[1];
+        TruffleAssert.passes(await BridgeInstance.adminAddRelayer(newRelayer));
+        assert.isTrue(await RelayerInstance.isRelayer(newRelayer))
+        TruffleAssert.passes(await BridgeInstance.adminRemoveRelayer(newRelayer));
+        assert.isFalse(await RelayerInstance.isRelayer(newRelayer));
+    });
+
     // Testing ownership methods
 
     it('Bridge admin should be expectedBridgeAdmin', async () => {
@@ -76,5 +87,29 @@ contract('Bridge - [admin]', async accounts => {
     it('Bridge admin should be set to zero address', async () => {
         TruffleAssert.passes(await BridgeInstance.renounceOwnership())
         assert.equal(await BridgeInstance.owner(), zeroAddress);
+    });
+
+    // Set resource ID
+
+    it('Should set a ERC20 Resource ID and contract address', async () => {
+        const ERC20MintableInstance = await ERC20MintableContract.new("token", "TOK");
+        const resourceID = Ethers.utils.hexZeroPad((ERC20MintableInstance.address + Ethers.utils.hexlify(chainID).substr(2)), 32);
+        const ERC20HandlerInstance = await ERC20HandlerContract.new(BridgeInstance.address, [], [], []);
+
+        TruffleAssert.passes(await BridgeInstance.adminSetResourceIDAndContractAddress(
+            ERC20HandlerInstance.address, resourceID, ERC20MintableInstance.address));
+        assert.equal(await ERC20HandlerInstance._resourceIDToTokenContractAddress.call(resourceID), ERC20MintableInstance.address);
+        assert.equal(await ERC20HandlerInstance._tokenContractAddressToResourceID.call(ERC20MintableInstance.address), resourceID.toLowerCase());
+    });
+
+    // Set burnable
+
+    it('Should set ERC20MintableInstance.address as burnable', async () => {
+        const ERC20MintableInstance = await ERC20MintableContract.new("token", "TOK");
+        const resourceID = Ethers.utils.hexZeroPad((ERC20MintableInstance.address + Ethers.utils.hexlify(chainID).substr(2)), 32);
+        const ERC20HandlerInstance = await ERC20HandlerContract.new(BridgeInstance.address, [resourceID], [ERC20MintableInstance.address], []);
+
+        TruffleAssert.passes(await BridgeInstance.adminSetBurnable(ERC20HandlerInstance.address, ERC20MintableInstance.address));
+        assert.isTrue(await ERC20HandlerInstance._burnList.call(ERC20MintableInstance.address));
     });
 });

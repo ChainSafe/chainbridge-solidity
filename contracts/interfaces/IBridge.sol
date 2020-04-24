1222842477ee3d868193433dfc2980075581a6dd
Wyatt Barnes <wyatt@writerof.software>
2020-04-24 02:05:43 -0500

    Add owner method

--- contracts/interfaces/IBridge.sol
+++ contracts/interfaces/IBridge.sol
@@ -1,8 +1,6 @@
 pragma solidity 0.6.4;
 
 interface IBridge {
-
-    // not sure if this should be done, exposing getter for _ChainID only
-    // better than using call
     function _chainID() external returns (uint8);
+    function owner() external returns (address);
 }

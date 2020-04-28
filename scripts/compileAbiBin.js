a255823e90a33e0b5363d29ca3a286db7bef67c7
Anthony Chernyak <anthony.chernyak@gmail.com>
2020-04-28 18:45:55 -0400

    Removing RuntimeBytecode (#102)
    - Removes adding the `runtimeBytecode` variable from `create_bindings.sh`
    - Removes the code that derived the bindings.

--- scripts/compileAbiBin.js
+++ scripts/compileAbiBin.js
@@ -39,12 +39,11 @@ fs.readdir("./build/contracts", function (err, files) {
         const path = './build/contracts/' + file
         let rawdata = fs.readFileSync(path);
         let contract = JSON.parse(rawdata);
-        let { abi, bytecode, deployedBytecode } = contract;
+        let { abi, bytecode} = contract;
         bytecode = bytecode.substring(2);
 
         if (abi.length === 0) return;
         fs.writeFileSync(ABI_PATH + basename + ".abi"  , JSON.stringify(abi));
         fs.writeFileSync(BIN_PATH + basename + ".bin", bytecode);
-        fs.writeFileSync(RUNTIME_PATH + basename + ".bin", deployedBytecode);
     });
 });

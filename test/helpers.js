827a4dd6de9b5e355719dc1748a1cfaf597ab6a2
steviezhang <stephanie@chainsafe.io>
2020-06-09 16:08:11 -0400

    add any bc of issue on ethers

--- test/helpers.js
+++ test/helpers.js
@@ -41,7 +41,7 @@ const createERC721DepositProposalData = (
 };
 
 const advanceBlock = () => {
-    let provider = new Ethers.providers.JsonRpcProvider("http://localhost:8545");
+    let provider = new Ethers.providers.Web3Provider(web3.currentProvider, "any");
     return provider.send("evm_mine", [new Date().getTime()])
 }
 

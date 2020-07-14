b3689bc605f39a55aea424a953cbf256d2e960be
Oleksii Matiiasevych <lastperson@gmail.com>
2020-07-14 16:24:37 +0300

    Enable optimization

--- truffle-config.js
+++ truffle-config.js
@@ -98,13 +98,13 @@ module.exports = {
     solc: {
       version: "0.6.4",       // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
       //  evmVersion: "byzantium"
-      // }
+      }
     }
   }
 }

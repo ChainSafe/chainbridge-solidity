945a2f5e817d6ac0c115b5e31bcef4e05b67cf56
Willem Olding <willemolding@gmail.com>
2021-03-26 13:01:25 +1100

    modify ovm truffle config to use optimism-integration

--- truffle-config-ovm.js
+++ truffle-config-ovm.js
@@ -7,9 +7,6 @@
  * Truffle config specifically for Optimistic Ethereum (OVM) 
  */
 
-const mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-const { ganache } = require('@eth-optimism/ovm-toolchain')
-
 const GAS_LIMIT = 9000000-1
 const GAS_PRICE = '0'
 
@@ -17,24 +14,7 @@ module.exports = {
   contracts_build_directory: './build/contracts/ovm',
   plugins: ["solidity-coverage"],
   networks: {
-
     test: {
-      network_id: 108,
-      networkCheckTimeout: 100000,
-      provider: function() {
-        return ganache.provider({
-          mnemonic: mnemonic,
-          network_id: 108,
-          default_balance_ether: 100,
-          gasLimit: GAS_LIMIT,
-          gasPrice: GAS_PRICE,
-        })
-      },
-      gas: GAS_LIMIT,
-      gasPrice: GAS_PRICE,
-    },
-
-    local: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "420",       // optimistic-integration default chain ID

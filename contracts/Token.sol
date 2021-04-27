349bca0d9610a2a440da226eb53404416e952662
Andrei Navoichyk <andrei@cere.io>
2021-04-27 17:13:03 +0300

    added Cere Token SC

---
+++ contracts/Token.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    uint8 private customDecimals;
+    uint256 public cap;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 _decimals,
+        uint256 _cap,
+        address[] memory _mintAddresses,
+        uint256[] memory _mintAmounts
+    )
+    ERC20(name, symbol)
+    {
+        require(_cap > 0, "Cannot have 0 total supply.");
+        require(_mintAddresses.length == _mintAmounts.length, "must have same number of mint addresses and amounts");
+        customDecimals = _decimals;
+        cap = _cap;
+
+        for (uint i; i < _mintAddresses.length; i++) {
+            require(_mintAddresses[i] != address(0), "Cannot have a non-address as reserve.");
+            require(totalSupply() + _mintAmounts[i] <= cap, "total supply of tokens cannot exceed the cap");
+            _mint(_mintAddresses[i], _mintAmounts[i]);
+        }
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return customDecimals;
+    }
+}

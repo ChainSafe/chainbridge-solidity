38ca9db07ef7fe75393f26d8abc0e4f5fdeacc09
Andrei Navoichyk <andrei@cere.io>
2021-04-28 14:40:00 +0300

    Token should be used instead OZ IERC20

--- contracts/ERC20Safe.sol
+++ contracts/ERC20Safe.sol
@@ -1,9 +1,9 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "./Token.sol";
 
 /**
     @title Manages deposited ERC20s.
@@ -20,7 +20,7 @@ contract ERC20Safe {
         @param amount Amount of tokens to transfer.
      */
     function fundERC20(address tokenAddress, address owner, uint256 amount) public {
-        IERC20 erc20 = IERC20(tokenAddress);
+        Token erc20 = Token(tokenAddress);
         _safeTransferFrom(erc20, owner, address(this), amount);
     }
 
@@ -32,7 +32,7 @@ contract ERC20Safe {
         @param amount Amount of tokens to transfer.
      */
     function lockERC20(address tokenAddress, address owner, address recipient, uint256 amount) internal {
-        IERC20 erc20 = IERC20(tokenAddress);
+        Token erc20 = Token(tokenAddress);
         _safeTransferFrom(erc20, owner, recipient, amount);
     }
 
@@ -43,7 +43,7 @@ contract ERC20Safe {
         @param amount Amount of tokens to transfer.
      */
     function releaseERC20(address tokenAddress, address recipient, uint256 amount) internal {
-        IERC20 erc20 = IERC20(tokenAddress);
+        Token erc20 = Token(tokenAddress);
         _safeTransfer(erc20, recipient, amount);
     }
 
@@ -76,7 +76,7 @@ contract ERC20Safe {
         @param to Address to transfer token to
         @param value Amount of token to transfer
      */
-    function _safeTransfer(IERC20 token, address to, uint256 value) private {
+    function _safeTransfer(Token token, address to, uint256 value) private {
         _safeCall(token, abi.encodeWithSelector(token.transfer.selector, to, value));
     }
 
@@ -88,7 +88,7 @@ contract ERC20Safe {
         @param to Address to transfer token to
         @param value Amount of token to transfer
      */
-    function _safeTransferFrom(IERC20 token, address from, address to, uint256 value) private {
+    function _safeTransferFrom(Token token, address from, address to, uint256 value) private {
         _safeCall(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
     }
 
@@ -97,7 +97,7 @@ contract ERC20Safe {
         @param token Token instance call targets
         @param data encoded call data
      */
-    function _safeCall(IERC20 token, bytes memory data) private {        
+    function _safeCall(Token token, bytes memory data) private {
         (bool success, bytes memory returndata) = address(token).call(data);
         require(success, "ERC20: call failed");
 

4efe0ebe8f146564bb9aeac157acf7b16be5d4a2
doasync <doasync@gmail.com>
2021-05-04 18:44:59 +0300

    add whitelist role and modifiers

--- contracts/utils/Whitelist.sol
+++ contracts/utils/Whitelist.sol
@@ -2,6 +2,19 @@
 
 pragma solidity >=0.6.0 <0.8.0;
 
-contract Whitelist {
+import "./AccessControl.sol";
 
+contract Whitelist is AccessControl {
+    bytes32 public constant WHITELIST_ROLE = keccak256("WHITELIST_ROLE");
+
+    modifier onlyWhitelistOrAdmin() {
+        _onlyWhitelistOrAdmin();
+        _;
+    }
+
+    function _onlyWhitelistOrAdmin() private view {
+        require(hasRole(WHITELIST_ROLE, msg.sender)
+            || hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            "sender doesn't have whitelist or admin role");
+    }
 }

a39006198866692a175ebd286894f739263cd348
Dan Kostiuk <daniel@nextgenbt.com>
2021-09-07 14:42:19 -0400

    pass in deconstructed fields to ProposalExecution event

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -70,9 +70,11 @@ contract Bridge is Pausable, AccessControl, SafeMath {
     );
 
     event ProposalExecution(
+      uint8   indexed originChainID,
       uint64  indexed depositNonce,
-      bytes32 indexed resourceID,
-      bytes data
+      address indexed recipientAddress,
+      bytes32 resourceID,
+      uint256 amount
     );
 
     bytes32 public constant RELAYER_ROLE = keccak256("RELAYER_ROLE");
@@ -420,7 +422,28 @@ contract Bridge is Pausable, AccessControl, SafeMath {
         depositHandler.executeProposal(proposal._resourceID, data);
 
         emit ProposalEvent(chainID, depositNonce, proposal._status, proposal._resourceID, proposal._dataHash);
-        emit ProposalExecution(depositNonce, proposal._resourceID, data);
+
+
+        // extract fields from data field for ERC20 (see ERC20Handler.executeProposal())
+        uint256 amount;
+        bytes memory destinationRecipientAddress;
+        bytes20 recipientAddress;
+
+        assembly {
+            amount := calldataload(0x64)
+            destinationRecipientAddress := mload(0x40)
+            let lenDestinationRecipientAddress := calldataload(0x84)
+            mstore(0x40, add(0x20, add(destinationRecipientAddress, lenDestinationRecipientAddress)))
+            // in the calldata the destinationRecipientAddress is stored at 0xC4 after accounting for the function signature and length declaration
+            calldatacopy(
+                destinationRecipientAddress, // copy to destinationRecipientAddress
+                0x84, // copy from calldata @ 0x84
+                sub(calldatasize(), 0x84) // copy size to the end of calldata
+            )
+            recipientAddress := mload(add(destinationRecipientAddress, 0x20))
+        }
+
+        emit ProposalExecution(chainID, depositNonce, address(recipientAddress), proposal._resourceID, amount);
     }
 
     /**

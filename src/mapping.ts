c498d128ed6b110ee4981850cac675ef75a37a71
Dan Kostiuk <daniel@nextgenbt.com>
2021-10-15 17:30:20 -0700

    added graph files

---
+++ src/mapping.ts
@@ -0,0 +1,10 @@
+import { ERC20ProposalExecuted } from '../generated/Contract/ERC20Handler'
+import { BridgeEvent } from '../generated/schema'
+
+export function handleERC20ProposalExecuted(event: ERC20ProposalExecuted): void {
+  const entity = new BridgeEvent(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
+  entity.address = event.params.recipientAddress
+  entity.amount = event.params.amount
+  entity.resourceId = event.params.resourceID
+  entity.save()
+}

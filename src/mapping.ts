0c1aa45225aea48335ad0748d733a76b9b0e1783
Dan Kostiuk <daniel@nextgenbt.com>
2021-10-20 10:14:11 -0400

    updated erc20handler contract entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,5 +6,7 @@ export function handleERC20ProposalExecuted(event: ERC20ProposalExecuted): void 
   entity.address = event.params.recipientAddress
   entity.amount = event.params.amount
   entity.resourceId = event.params.resourceID
+  entity.txHash = event.transaction.hash
+  entity.timestamp = Date.now()
   entity.save()
 }

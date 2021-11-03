6ed0086d929cc789aebcbb3b3df18fabeaf94452
andersonlee725 <anderson@chainsafe.io>
2021-11-03 12:23:07 -0700

    modify withdraw test and add tests for erc1155

---
+++ test/e2e/erc1155/differentChainsMock.js
@@ -0,0 +1,181 @@
+const TruffleAssert = require('truffle-assertions');
+const Ethers = require('ethers');
+
+const Helpers = require('../../helpers');
+
+const BridgeContract = artifacts.require("Bridge");
+const ERC1155MintableContract = artifacts.require("ERC1155PresetMinterPauser");
+const ERC1155HandlerContract = artifacts.require("ERC1155Handler");
+
+contract('E2E ERC1155 - Two EVM Chains', async accounts => {
+    const originRelayerThreshold = 2;
+    const originDomainID = 1;
+    const originRelayer1Address = accounts[3];
+    const originRelayer2Address = accounts[4];
+
+    const destinationRelayerThreshold = 2;
+    const destinationDomainID = 2;
+    const destinationRelayer1Address = accounts[3];
+    const destinationRelayer2Address = accounts[4];
+
+    const depositerAddress = accounts[1];
+    const recipientAddress = accounts[2];
+    const tokenID = 1;
+    const initialTokenAmount = 100;
+    const depositAmount = 10; 
+    const expectedDepositNonce = 1;
+
+    let OriginBridgeInstance;
+    let OriginERC1155MintableInstance;
+    let OriginERC1155HandlerInstance
+    let originDepositData;
+    let originDepositProposalData;
+    let originResourceID;
+    let originBurnableContractAddresses;
+
+    let DestinationBridgeInstance;
+    let DestinationERC1155MintableInstance;
+    let DestinationERC1155HandlerInstance;
+    let destinationDepositData;
+    let destinationDepositProposalData;
+    let destinationResourceID;
+    let destinationBurnableContractAddresses;
+
+    beforeEach(async () => {
+        await Promise.all([
+            BridgeContract.new(originDomainID, [originRelayer1Address, originRelayer2Address], originRelayerThreshold, 0, 100).then(instance => OriginBridgeInstance = instance),
+            BridgeContract.new(destinationDomainID, [destinationRelayer1Address, destinationRelayer2Address], destinationRelayerThreshold, 0, 100).then(instance => DestinationBridgeInstance = instance),
+            ERC1155MintableContract.new("TOK").then(instance => OriginERC1155MintableInstance = instance),
+            ERC1155MintableContract.new("TOK").then(instance => DestinationERC1155MintableInstance = instance)
+        ]);
+        
+        originResourceID = Helpers.createResourceID(OriginERC1155MintableInstance.address, originDomainID);
+        originInitialResourceIDs = [originResourceID];
+        originInitialContractAddresses = [OriginERC1155MintableInstance.address];
+        originBurnableContractAddresses = [];
+        
+        destinationResourceID = Helpers.createResourceID(DestinationERC1155MintableInstance.address, originDomainID)
+        destinationInitialResourceIDs = [destinationResourceID];
+        destinationInitialContractAddresses = [DestinationERC1155MintableInstance.address];
+        destinationBurnableContractAddresses = [DestinationERC1155MintableInstance.address];
+
+        await Promise.all([
+            ERC1155HandlerContract.new(OriginBridgeInstance.address)
+                .then(instance => OriginERC1155HandlerInstance = instance),
+            ERC1155HandlerContract.new(DestinationBridgeInstance.address)
+                .then(instance => DestinationERC1155HandlerInstance = instance)
+        ]);
+
+        await OriginERC1155MintableInstance.mintBatch(depositerAddress, [tokenID], [initialTokenAmount], "0x0");
+
+        await Promise.all([
+            OriginERC1155MintableInstance.setApprovalForAll(OriginERC1155HandlerInstance.address, true, { from: depositerAddress }),
+            DestinationERC1155MintableInstance.grantRole(await DestinationERC1155MintableInstance.MINTER_ROLE(), DestinationERC1155HandlerInstance.address),
+            OriginBridgeInstance.adminSetResource(OriginERC1155HandlerInstance.address, originResourceID, OriginERC1155MintableInstance.address),
+            DestinationBridgeInstance.adminSetResource(DestinationERC1155HandlerInstance.address, destinationResourceID, DestinationERC1155MintableInstance.address),
+            DestinationBridgeInstance.adminSetBurnable(DestinationERC1155HandlerInstance.address, destinationBurnableContractAddresses[0])
+        ]);
+        
+        originDepositData = Helpers.createERC1155DepositData([tokenID], [depositAmount]);
+        originDepositProposalData = Helpers.createERC1155DepositProposalData([tokenID], [depositAmount], recipientAddress, "0x0");
+
+        destinationDepositData = Helpers.createERC1155DepositData([tokenID], [depositAmount]);
+        destinationDepositProposalData = Helpers.createERC1155DepositProposalData([tokenID], [depositAmount], depositerAddress, "0x0");
+    });
+
+    it("[sanity] depositerAddress' balance of tokenID should be equal to initialTokenAmount", async () => {
+        const depositerBalance = await OriginERC1155MintableInstance.balanceOf(depositerAddress, tokenID);
+        assert.strictEqual(depositerBalance.toNumber(), initialTokenAmount);
+    });
+
+    it("[sanity] DestinationERC1155HandlerInstance.address should have minterRole for DestinationERC1155MintableInstance", async () => {
+        const isMinter = await DestinationERC1155MintableInstance.hasRole(await DestinationERC1155MintableInstance.MINTER_ROLE(), DestinationERC1155HandlerInstance.address);
+        assert.isTrue(isMinter);
+    });
+
+    it("E2E: tokenID of Origin ERC1155 owned by depositAddress to Destination ERC1155 owned by recipientAddress and back again", async () => {
+        let tokenOwner;
+
+        let depositerBalance;
+        let recipientBalance;
+
+        // depositerAddress makes initial deposit of tokenID
+        await TruffleAssert.passes(OriginBridgeInstance.deposit(
+            destinationDomainID,
+            originResourceID,
+            originDepositData,
+            { from: depositerAddress }
+        ));
+
+        depositerBalance = await OriginERC1155MintableInstance.balanceOf(depositerAddress, tokenID);
+        assert.strictEqual(depositerBalance.toNumber(), initialTokenAmount - depositAmount);
+
+        // // destinationRelayer1 creates the deposit proposal
+        await TruffleAssert.passes(DestinationBridgeInstance.voteProposal(
+            originDomainID,
+            expectedDepositNonce,
+            destinationResourceID,
+            originDepositProposalData,
+            { from: destinationRelayer1Address }
+        ));
+
+        // // destinationRelayer2 votes in favor of the deposit proposal
+        // // because the destinationRelayerThreshold is 2, the deposit proposal will go
+        // // into a finalized state
+        // // and then automatically executes the proposal
+        await TruffleAssert.passes(DestinationBridgeInstance.voteProposal(
+            originDomainID,
+            expectedDepositNonce,
+            destinationResourceID,
+            originDepositProposalData,
+            { from: destinationRelayer2Address }
+        ));
+
+        depositerBalance = await OriginERC1155MintableInstance.balanceOf(depositerAddress, tokenID);
+        assert.strictEqual(depositerBalance.toNumber(), initialTokenAmount - depositAmount, "depositAmount wasn't transferred from depositerAddress");
+
+        recipientBalance = await DestinationERC1155MintableInstance.balanceOf(recipientAddress, tokenID);
+        assert.strictEqual(recipientBalance.toNumber(), depositAmount, "depositAmount wasn't transferred to recipientAddress");
+
+        await DestinationERC1155MintableInstance.setApprovalForAll(DestinationERC1155HandlerInstance.address, true, { from: recipientAddress });
+
+        // recipientAddress makes a deposit of the received depositAmount
+        await TruffleAssert.passes(DestinationBridgeInstance.deposit(
+            originDomainID,
+            destinationResourceID,
+            destinationDepositData,
+            { from: recipientAddress }
+        ));
+
+        // Recipient should have a balance of 0 (deposit amount - deposit amount)
+        recipientBalance = await DestinationERC1155MintableInstance.balanceOf(recipientAddress, tokenID);
+        assert.strictEqual(recipientBalance.toNumber(), 0);
+
+        // destinationRelayer1 creates the deposit proposal
+        await TruffleAssert.passes(OriginBridgeInstance.voteProposal(
+            destinationDomainID,
+            expectedDepositNonce,
+            originResourceID,
+            destinationDepositProposalData,
+            { from: originRelayer1Address }
+        ));
+
+        // destinationRelayer2 votes in favor of the deposit proposal
+        // because the destinationRelayerThreshold is 2, the deposit proposal will go
+        // into a finalized state
+        // and then automatically executes the proposal
+        await TruffleAssert.passes(OriginBridgeInstance.voteProposal(
+            destinationDomainID,
+            expectedDepositNonce,
+            originResourceID,
+            destinationDepositProposalData,
+            { from: originRelayer2Address }
+        ));
+
+        recipientBalance = await DestinationERC1155MintableInstance.balanceOf(recipientAddress, tokenID);
+        assert.strictEqual(recipientBalance.toNumber(), 0);
+        
+        depositerBalance = await OriginERC1155MintableInstance.balanceOf(depositerAddress, tokenID);
+        assert.strictEqual(depositerBalance.toNumber(), initialTokenAmount);
+    });
+});
--- test/gasBenchmarks/executeProposal.js
+++ test/gasBenchmarks/executeProposal.js
@@ -10,7 +10,9 @@ const BridgeContract = artifacts.require("Bridge");
 const ERC20HandlerContract = artifacts.require("ERC20Handler");
 const ERC20MintableContract = artifacts.require("ERC20PresetMinterPauser");
 const ERC721HandlerContract = artifacts.require("ERC721Handler");
+const ERC1155HandlerContract = artifacts.require("ERC1155Handler");
 const ERC721MintableContract = artifacts.require("ERC721MinterBurnerPauser");
+const ERC1155MintableContract = artifacts.require("ERC1155PresetMinterPauser");
 const GenericHandlerContract = artifacts.require("GenericHandler");
 const CentrifugeAssetContract = artifacts.require("CentrifugeAsset");
 const NoArgumentContract = artifacts.require("NoArgument");
@@ -30,12 +32,15 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
     const initialRelayers = [relayerAddress];
     const erc20TokenAmount = 100;
     const erc721TokenID = 1;
+    const erc1155TokenID = 1;
+    const erc1155TokenAmount = 100;
 
     let BridgeInstance;
     let ERC20MintableInstance;
     let ERC20HandlerInstance;
     let ERC721MintableInstance;
     let ERC721HandlerInstance;
+    let ERC1155HandlerInstance;
     let CentrifugeAssetInstance;
     let NoArgumentInstance;
     let OneArgumentInstance;
@@ -44,6 +49,7 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
 
     let erc20ResourceID;
     let erc721ResourceID;
+    let erc1155ResourceID;
     let centrifugeAssetResourceID;
     let noArgumentResourceID;
     let oneArgumentResourceID;
@@ -59,6 +65,7 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
             BridgeContract.new(domainID, initialRelayers, relayerThreshold, 0, 100).then(instance => BridgeInstance = instance),
             ERC20MintableContract.new("token", "TOK").then(instance => ERC20MintableInstance = instance),
             ERC721MintableContract.new("token", "TOK", "").then(instance => ERC721MintableInstance = instance),
+            ERC1155MintableContract.new("TOK").then(instance => ERC1155MintableInstance = instance),
             CentrifugeAssetContract.new().then(instance => CentrifugeAssetInstance = instance),
             NoArgumentContract.new().then(instance => NoArgumentInstance = instance),
             OneArgumentContract.new().then(instance => OneArgumentInstance = instance),
@@ -68,6 +75,7 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
 
         erc20ResourceID = Helpers.createResourceID(ERC20MintableInstance.address, domainID);
         erc721ResourceID = Helpers.createResourceID(ERC721MintableInstance.address, domainID);
+        erc1155ResourceID = Helpers.createResourceID(ERC1155MintableInstance.address, domainID);
         centrifugeAssetResourceID = Helpers.createResourceID(CentrifugeAssetInstance.address, domainID);
         noArgumentResourceID = Helpers.createResourceID(NoArgumentInstance.address, domainID);
         oneArgumentResourceID = Helpers.createResourceID(OneArgumentInstance.address, domainID);
@@ -110,14 +118,18 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
             ERC20MintableInstance.mint(depositerAddress, erc20TokenAmount),
             ERC721HandlerContract.new(BridgeInstance.address).then(instance => ERC721HandlerInstance = instance),
             ERC721MintableInstance.mint(depositerAddress, erc721TokenID, ""),
+            ERC1155HandlerContract.new(BridgeInstance.address).then(instance => ERC1155HandlerInstance = instance),
+            ERC1155MintableInstance.mintBatch(depositerAddress, [erc1155TokenID], [erc1155TokenAmount], "0x0"),
             GenericHandlerInstance = await GenericHandlerContract.new(BridgeInstance.address)
         ]);
 
         await Promise.all([
             ERC20MintableInstance.approve(ERC20HandlerInstance.address, erc20TokenAmount, { from: depositerAddress }),
             ERC721MintableInstance.approve(ERC721HandlerInstance.address, erc721TokenID, { from: depositerAddress }),
+            ERC1155MintableInstance.setApprovalForAll(ERC1155HandlerInstance.address, true, { from: depositerAddress }),
             BridgeInstance.adminSetResource(ERC20HandlerInstance.address, erc20ResourceID, ERC20MintableInstance.address),
             BridgeInstance.adminSetResource(ERC721HandlerInstance.address, erc721ResourceID, ERC721MintableInstance.address),
+            BridgeInstance.adminSetResource(ERC1155HandlerInstance.address, erc1155ResourceID, ERC1155MintableInstance.address),
             BridgeInstance.adminSetGenericResource(GenericHandlerInstance.address, centrifugeAssetResourceID, genericInitialContractAddresses[0], genericInitialDepositFunctionSignatures[0], genericInitialDepositFunctionDepositerOffsets[0], genericInitialExecuteFunctionSignatures[0]),
             BridgeInstance.adminSetGenericResource(GenericHandlerInstance.address, noArgumentResourceID, genericInitialContractAddresses[1], genericInitialDepositFunctionSignatures[1], genericInitialDepositFunctionDepositerOffsets[1], genericInitialExecuteFunctionSignatures[1]),
             BridgeInstance.adminSetGenericResource(GenericHandlerInstance.address, oneArgumentResourceID, genericInitialContractAddresses[2], genericInitialDepositFunctionSignatures[2], genericInitialDepositFunctionDepositerOffsets[2], genericInitialExecuteFunctionSignatures[2]),
@@ -162,8 +174,22 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
         });
     });
 
-    it('Should execute Generic deposit proposal - Centrifuge asset', async () => {
+    it('Should execute ERC1155 deposit proposal', async () => {
         const depositNonce = 3;
+        
+        const depositData = Helpers.createERC1155DepositData([erc1155TokenID], [erc1155TokenAmount]);
+
+        await deposit(erc1155ResourceID, depositData);
+        const voteWithExecuteTx = await vote(erc1155ResourceID, depositNonce, depositData, relayerAddress);
+
+        gasBenchmarks.push({
+            type: 'ERC1155',
+            gasUsed: voteWithExecuteTx.receipt.gasUsed
+        });
+    });
+
+    it('Should execute Generic deposit proposal - Centrifuge asset', async () => {
+        const depositNonce = 4;
         const hashOfCentrifugeAsset = Ethers.utils.keccak256('0xc0ffee');
         const depositData = Helpers.createGenericDepositData(hashOfCentrifugeAsset);
 
@@ -177,7 +203,7 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
     });
 
     it('Should execute Generic deposit proposal - No Argument', async () => {
-        const depositNonce = 4;
+        const depositNonce = 5;
         const depositData = Helpers.createGenericDepositData(null);
 
         await deposit(noArgumentResourceID, depositData);
@@ -190,7 +216,7 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
     });
 
     it('Should make Generic deposit - One Argument', async () => {
-        const depositNonce = 5;
+        const depositNonce = 6;
         const depositData = Helpers.createGenericDepositData(Helpers.toHex(42, 32));
 
         await deposit(oneArgumentResourceID, depositData);
@@ -203,7 +229,7 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
     });
 
     it('Should make Generic deposit - Two Arguments', async () => {
-        const depositNonce = 6;
+        const depositNonce = 7;
         const argumentOne = [NoArgumentInstance.address, OneArgumentInstance.address, TwoArgumentsInstance.address];
         const argumentTwo = Helpers.getFunctionSignature(CentrifugeAssetInstance, 'store');
         const encodedMetaData = Helpers.abiEncode(['address[]','bytes4'], [argumentOne, argumentTwo]);
@@ -219,7 +245,7 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
     });
 
     it('Should make Generic deposit - Three Arguments', async () => {
-        const depositNonce = 7;
+        const depositNonce = 8;
         const argumentOne = 'soylentGreenIsPeople';
         const argumentTwo = -42;
         const argumentThree = true;

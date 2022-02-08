9c185ec9950bfa59e4c00bdc865c3fa3196d2e7f
andersonlee725 <anderson@chainsafe.io>
2022-02-08 05:14:47 -0800

    fix comment

--- test/gasBenchmarks/executeProposal.js
+++ test/gasBenchmarks/executeProposal.js
@@ -156,8 +156,8 @@ contract('Gas Benchmark - [Execute Proposal]', async (accounts) => {
 
     it('Should execute ERC721 deposit proposal', async () => {
         const depositNonce = 2;
-        const lenMetaData = 1;
-        const metaData = 0;
+        const lenMetaData = 0;
+        const metaData = "0x";
         const depositData = Helpers.createERC721DepositProposalData(
             erc721TokenID,
             lenRecipientAddress,

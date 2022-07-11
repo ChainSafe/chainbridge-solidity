355a1fe9afe314d839410d9a900989f2653d4e85
Nikola <nikola.mlinaric93@gmail.com>
2022-07-11 13:58:02 +0200

    Improve onlyAllowed modifier descripion

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -106,7 +106,8 @@ contract Bridge is Pausable, Context {
 
     /**
         @notice Pauses deposits, proposal creation and voting, and deposit executions.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
      */
     function adminPauseTransfers() external onlyAllowed {
         _pause(_msgSender());
@@ -114,7 +115,8 @@ contract Bridge is Pausable, Context {
 
     /**
         @notice Unpauses deposits, proposal creation and voting, and deposit executions.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @notice MPC address has to be set before Bridge can be unpaused
      */
     function adminUnpauseTransfers() external onlyAllowed {
@@ -125,7 +127,8 @@ contract Bridge is Pausable, Context {
     /**
         @notice Sets a new resource for handler contracts that use the IERCHandler interface,
         and maps the {handlerAddress} to {resourceID} in {_resourceIDToHandlerAddress}.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @param handlerAddress Address of handler resource will be set for.
         @param resourceID ResourceID to be used when making deposits.
         @param tokenAddress Address of contract to be called when a deposit is made and a deposited is executed.
@@ -139,7 +142,8 @@ contract Bridge is Pausable, Context {
     /**
         @notice Sets a new resource for handler contracts that use the IGenericHandler interface,
         and maps the {handlerAddress} to {resourceID} in {_resourceIDToHandlerAddress}.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @param handlerAddress Address of handler resource will be set for.
         @param resourceID ResourceID to be used when making deposits.
         @param contractAddress Address of contract to be called when a deposit is made and a deposited is executed.
@@ -159,7 +163,8 @@ contract Bridge is Pausable, Context {
 
     /**
         @notice Sets a resource as burnable for handler contracts that use the IERCHandler interface.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @param handlerAddress Address of handler resource will be set for.
         @param tokenAddress Address of contract to be called when a deposit is made and a deposited is executed.
      */
@@ -170,7 +175,8 @@ contract Bridge is Pausable, Context {
 
     /**
         @notice Sets the nonce for the specific domainID.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @param domainID Domain ID for increasing nonce.
         @param nonce The nonce value to be set.
      */
@@ -181,7 +187,8 @@ contract Bridge is Pausable, Context {
 
     /**
         @notice Set a forwarder to be used.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @param forwarder Forwarder address to be added.
         @param valid Decision for the specific forwarder.
      */
@@ -191,7 +198,8 @@ contract Bridge is Pausable, Context {
 
     /**
         @notice Changes access control contract address.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @param newAccessControl Address {_accessControl} will be updated to.
      */
     function adminChangeAccessControl(address newAccessControl) external onlyAllowed {
@@ -201,7 +209,8 @@ contract Bridge is Pausable, Context {
 
     /**
         @notice Changes deposit fee handler contract address.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @param newFeeHandler Address {_feeHandler} will be updated to.
      */
     function adminChangeFeeHandler(address newFeeHandler) external onlyAllowed {
@@ -211,7 +220,8 @@ contract Bridge is Pausable, Context {
 
     /**
         @notice Used to manually withdraw funds from ERC safes.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @param handlerAddress Address of handler to withdraw from.
         @param data ABI-encoded withdrawal params relevant to the specified handler.
      */
@@ -327,7 +337,8 @@ contract Bridge is Pausable, Context {
     /**
         @notice Once MPC address is set, this method can't be invoked anymore.
         It's used to trigger the belonging process on the MPC side which also handles keygen function calls order.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
      */
     function startKeygen() external onlyAllowed {
         require(_MPCAddress == address(0), "MPC address is already set");
@@ -337,7 +348,8 @@ contract Bridge is Pausable, Context {
     /**
         @notice This method can be called only once, after the MPC address is set Bridge is unpaused.
         It's used to trigger the belonging process on the MPC side which also handles keygen function calls order.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
         @param MPCAddress Address that will be set as MPC address.
      */
     function endKeygen(address MPCAddress) external onlyAllowed {
@@ -351,7 +363,8 @@ contract Bridge is Pausable, Context {
     /**
         @notice It's used to trigger the belonging process on the MPC side.
         It's used to trigger the belonging process on the MPC side which also handles keygen function calls order.
-        @notice Only callable by address that has function call access.
+        @notice Only callable by address that has the right to call the specific function,
+        which is mapped in {functionAccess} in AccessControlSegregator contract.
      */
     function refreshKey() external onlyAllowed {
         emit KeyRefresh();
